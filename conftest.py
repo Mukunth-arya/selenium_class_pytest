24671162901729435e8c3180207d00c183cee05d
= <mukunthnataraj@gmail.com>
2022-03-22 14:34:26 +0530

    latest

---
+++ conftest.py
@@ -0,0 +1,19 @@
+import pytest
+from selenium import webdriver
+
+
+
+@pytest.fixture(params = ["chrome"], scope= "class")
+def init_driver(request):
+   print("here comes the the sample testing")
+   if request.param == "chrome":
+    options = webdriver.ChromeOptions()
+    options.add_argument("start-maximized")
+    web_driver = webdriver.Chrome(executable_path= "C:\\Users\\mukunth\\PycharmProjects\\pythonProject\\testing_pytest\\chromedriver.exe",options=options )
+    web_driver.implicitly_wait(10)
+    web_driver.delete_all_cookies()
+    request.cls.driver = web_driver
+   else:
+       print("please enter the proper driver details")
+   yield
+   web_driver.close()
